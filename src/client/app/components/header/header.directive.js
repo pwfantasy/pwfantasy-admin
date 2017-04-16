4b81256f22cb29e3191cc8b95df135ac7382fa0c
sean3z <seanwragg@gmail.com>
2017-04-16 13:01:29 -0400

    removing header ctrl and adding more states

--- src/client/app/components/header/header.directive.js
+++ src/client/app/components/header/header.directive.js
@@ -8,8 +8,6 @@
     function Header() {
         return {
             restrict: 'E',
-            controller: 'HeaderCtrl',
-            controllerAs: 'headerVm',
             templateUrl: '/app/components/header/header.view.html'
         };
     }
